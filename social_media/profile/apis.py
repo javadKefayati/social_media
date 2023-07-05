682e442b5b722becb1353d3a626971e757b62946
javad <javad936807@gmail.com>
2023-07-05 15:13:38 +0330

    feat: Write get api profile list

---
+++ social_media/profile/apis.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+
+from django.core.validators import MinLengthValidator
+from .validators import number_validator, special_char_validator, letter_validator
+from social_media.users.models import BaseUser , Profile
+from social_media.api.mixins import ApiAuthMixin
+from social_media.users.selectors import get_profile
+from social_media.users.services import register 
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+
+from drf_spectacular.utils import extend_schema
+
+
+class ProfileApi(ApiAuthMixin, APIView):
+
+    class OutPutSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Profile 
+            fields = ("bio", "posts_count", "follower_count", "following_count")
+
+    @extend_schema(responses=OutPutSerializer)
+    def get(self, request):
+        query = get_profile(user=request.user)
+        return Response(self.OutPutSerializer(query, context={"request":request}).data)
