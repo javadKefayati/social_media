1a19b90085686cf2c30b17e1b7143305b643d018
javad <javad936807@gmail.com>
2023-07-05 15:09:11 +0330

    feat: Write Profile model

--- social_media/profile/models.py
+++ social_media/profile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from social_media.users.models import BaseUser
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    
+    posts_count = models.PositiveIntegerField(default=0)
+    follower_count = models.PositiveIntegerField(default=0)
+    following_count = models.PositiveIntegerField(default=0)
+    bio = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} >> {self.bio}"
