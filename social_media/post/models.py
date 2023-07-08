d35aff92bff9663c584c83018af8dfac374bc3aa
javad <javad936807@gmail.com>
2023-07-08 11:47:35 +0330

    fix: chagne address of profile model in post model

--- social_media/post/models.py
+++ social_media/post/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from social_media.common.models import BaseModel
-from social_media.users.models import Profile
+from social_media.profile.models import Profile
 class Post(BaseModel):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField(
