2fafc7022b92ced0972fd654e9072fe1a627222f
javad <javad936807@gmail.com>
2023-07-10 18:58:39 +0330

    feat: finish post endpoints

--- social_media/api/urls.py
+++ social_media/api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
-    # path('blog/', include(('social_media.blog.urls', 'blog')))
+    path("schema/", SpectacularAPIView.as_view(api_version="v1"), name="schema"),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('auth/', include(('social_media.authentication.urls', 'auth'))),
     path('user/', include(('social_media.users.urls', 'register'))),
     path('profile/', include(('social_media.profile.urls', 'profile'))),
-    path('post/', include(('social_media.post.urls', 'post'))),
+    path('post/', include(('social_media.post.urls', 'post')),name="post apis"),
 ]
--- social_media/post/urls.py
+++ social_media/post/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .apis import PostList
 from rest_framework import routers
 router = routers.DefaultRouter()
-router.register(r'cutarea', PostList,    basename='cutareadel')
+router.register(r'', PostList,    basename='cutareadel')
 
 urlpatterns = [
     # url(r'^apidsf/', include(router.urls)),
-    # path('', PostList.as_view({'get': 'list'}),name="post api1"),
+    # path('', PostList.as_view(),name="post api1"),
 ]
 urlpatterns += router.urls
