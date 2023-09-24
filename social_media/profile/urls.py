a24a54fd05a42be576ad74dc5690c08531f09690
javadKefayati <javad936807@gmail.com>
2023-09-24 17:57:18 +0330

    fix: Change name of /api/profile/get to /api/profile/info

--- social_media/profile/urls.py
+++ social_media/profile/urls.py
@@ -4,7 +4,7 @@ from social_media.users import RegisterProfileApi
 
 
 urlpatterns = [
-    path('get/', ProfileApi.as_view(),name="get profile "),
+    path('info/', ProfileApi.as_view(),name="get profile "),
     path('register/', RegisterProfileApi.as_view(),name="register"),
 
 ]
