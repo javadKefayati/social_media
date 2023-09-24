d4e9866a5dd9d0d05cb9ad1e0528fed91991cf32
javadKefayati <javad936807@gmail.com>
2023-09-24 17:53:32 +0330

    bug: Change name of api/user/register/ to api/profile/register

--- social_media/profile/urls.py
+++ social_media/profile/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .apis import ProfileApi
+from social_media.users import RegisterApi
 
 
 urlpatterns = [
     path('get/', ProfileApi.as_view(),name="get profile "),
+    path('register/', RegisterApi.as_view(),name="register"),
+
 ]
